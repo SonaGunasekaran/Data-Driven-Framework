5b48148642fe00666366921848cb18c64f23dabb
[Sona] <sona.g@tvsnext.io>
2021-09-14 15:15:42 +0530

    [Sona] Modified : Data driven framework is implemented using CSv file

--- FBDatadriven/LoginPage/FbLoginPage.cs
+++ FBDatadriven/LoginPage/FbLoginPage.cs
@@ -3,31 +3,49 @@
  * Author:Sona G
  * Date :12/09/2021
  */
-using System;
+using OpenQA.Selenium;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+
 
 namespace FBDatadriven.LoginPage
 {
-    public class FbLoginPage : UserAdapter
+    public class FbLoginPage : Base.Baseclass
     {
         string[] userData;
-        Dictionary<string, UserData> dataMap;
-        public Dictionary<string, UserData> LoadCensusData(string csvFilePath, string dataHeader)
+        Dictionary<string, UserData> user;
+        public Dictionary<string, UserData> LoadUserData(string csvFilePath, string dataHeader)
         {
-            dataMap = new Dictionary<string, UserData>();
-            userData = GetUserData(csvFilePath, dataHeader);
+            //Read all lines from csv file
+            userData = File.ReadAllLines(csvFilePath);
+
+            user = new Dictionary<string, UserData>();
+            //Skip the header
             foreach (string data in userData.Skip(1))
             {
-                if (!data.Contains(","))
-                    throw new UserDataException("File contains invalid Delimiter", UserDataException.ExceptionType.INCORRECT_DELIMITER);
+                //Split the column by comma
                 string[] column = data.Split(",");
                 if (csvFilePath.Contains("FBfile.csv"))
-                    dataMap.Add(column[0],(new UserData(column[0], column[1], column[2])));
+                {
+                    //Add the column to the dictionary
+                    user.Add(column[0], new UserData(column[0], column[1]));
+
+                    //Find the element by name and pass the value
+                    driver.FindElement(By.Name("email")).SendKeys(column[0]);
+                    System.Threading.Thread.Sleep(1000);
+
+                    //Find the element by Id and pass the value
+                    driver.FindElement(By.Id("pass")).SendKeys(column[1]);
+                    System.Threading.Thread.Sleep(1000);
+
+                    driver.FindElement(By.Name("login")).Click();
+                    System.Threading.Thread.Sleep(1000);
+                }
             }
-            return dataMap;
+            return user;
         }
     }
 
 }
+
