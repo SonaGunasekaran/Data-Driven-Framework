155f2781091c0b997d07cefa8b5555fc41b7646a
[Sona] <sona.g@tvsnext.io>
2021-09-15 15:42:54 +0530

    [Sona] Data Driven Framework is implemented using CSV file

--- FBDatadriven/LoginPage/FbLoginPage.cs
+++ FBDatadriven/LoginPage/FbLoginPage.cs
@@ -4,47 +4,28 @@
  * Date :12/09/2021
  */
 using OpenQA.Selenium;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-
+using SeleniumExtras.PageObjects;
 
 namespace FBDatadriven.LoginPage
 {
-    public class FbLoginPage : Base.Baseclass
+    public class FbLoginPage 
     {
-        string[] userData;
-        Dictionary<string, UserData> user;
-        public Dictionary<string, UserData> LoadUserData(string csvFilePath, string dataHeader)
+        public FbLoginPage(IWebDriver driver)
         {
-            //Read all lines from csv file
-            userData = File.ReadAllLines(csvFilePath);
-
-            user = new Dictionary<string, UserData>();
-            //Skip the header
-            foreach (string data in userData.Skip(1))
-            {
-                //Split the column by comma
-                string[] column = data.Split(",");
-                if (csvFilePath.Contains("FBfile.csv"))
-                {
-                    //Add the column to the dictionary
-                    user.Add(column[0], new UserData(column[0], column[1]));
-
-                    //Find the element by name and pass the value
-                    driver.FindElement(By.Name("email")).SendKeys(column[0]);
-                    System.Threading.Thread.Sleep(1000);
-
-                    //Find the element by Id and pass the value
-                    driver.FindElement(By.Id("pass")).SendKeys(column[1]);
-                    System.Threading.Thread.Sleep(1000);
-
-                    driver.FindElement(By.Name("login")).Click();
-                    System.Threading.Thread.Sleep(1000);
-                }
-            }
-            return user;
+            PageFactory.InitElements(driver, this);
         }
+
+        [FindsBy(How = How.Name, Using = "email")]
+        [CacheLookup]
+        public IWebElement email;
+
+        [FindsBy(How = How.Name, Using = "pass")]
+        [CacheLookup]
+        public IWebElement password;
+
+        [FindsBy(How = How.Name, Using = "login")]
+        [CacheLookup]
+        public IWebElement loginbtn;
     }
 
 }
--- FBDatadriven/UnitTest1.cs
+++ FBDatadriven/UnitTest1.cs
@@ -3,26 +3,19 @@
  * Author:Sona G
  * Date :12/09/2021
  */
-using FBDatadriven.LoginPage;
 using NUnit.Framework;
-using System.Collections.Generic;
 
 namespace FBDatadriven
 {
     public class Tests : Base.Baseclass
     {
-        Dictionary<string, UserData> userRecord= new Dictionary<string, UserData>();
         string csvFilePath = @"C:\Users\sona.g\source\repos\FBDatadriven\FBDatadriven\CsvFile\FBfile.csv";
-        LoginPage.FbLoginPage login=new LoginPage.FbLoginPage();
-
+        DoAction.DoActions action =new DoAction.DoActions();
+        
         [Test]
         public void Test1()
         {
-            userRecord =login.LoadUserData(csvFilePath,"Email,Password");
-            //int actual = userRecord.Count;
-            //int expected = 3;
-            //assertion
-            //Assert.AreEqual(actual, expected);
+            action.LoadUserData(csvFilePath,"Email,Password");
         }
     }
 }
