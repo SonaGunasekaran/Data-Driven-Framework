9da4d87d7d4c667d126bb7f77d03bdc32a4d021a
[Sona] <sona.g@tvsnext.io>
2021-09-20 11:14:57 +0530

    [Sona] Modified : Implementing extend report

---
+++ FBDatadriven/ExtendReport/ExtendReportclass.cs
@@ -0,0 +1,35 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FBDatadriven.ExtendReport
+{
+    [TestFixture]
+    public class ExtendReportclass
+    {
+        public static ExtentHtmlReporter htmlReporter;
+        public static ExtentReports extent;
+        public static ExtentTest test;
+        public static ExtentReports report()
+        {
+            if (extent == null)
+            {
+                string reportPath = @"C:\Users\sona.g\source\repos\FBDatadriven\FBDatadriven\ExtendReport\ExtendReports.html";
+                htmlReporter = new ExtentHtmlReporter(reportPath);
+                extent = new ExtentReports();
+                extent.AttachReporter(htmlReporter);
+                extent.AddSystemInfo("OS", "Windows");
+                extent.AddSystemInfo("UserName", "Sona");
+                extent.AddSystemInfo("ProviderName", "Sona");
+                extent.AddSystemInfo("Domain", "QA");
+                extent.AddSystemInfo("ProjectName", "Facebook Automation");
+                string conifgPath = @"C:\Users\sona.g\source\repos\FBDatadriven\FBDatadriven\ExtendReport\extend-config.xml";
+                htmlReporter.LoadConfig(conifgPath);
+            }
+            return extent;
+        }
+    }
+}
--- FBDatadriven/UnitTest1.cs
+++ FBDatadriven/UnitTest1.cs
@@ -3,19 +3,32 @@
  * Author:Sona G
  * Date :12/09/2021
  */
+using AventStack.ExtentReports;
+using FBDatadriven.ExtendReport;
 using NUnit.Framework;
 
 namespace FBDatadriven
 {
+    
     public class Tests : Base.Baseclass
     {
+        ExtentReports reports = ExtendReportclass.report();
+        ExtentTest test;
         string csvFilePath = @"C:\Users\sona.g\source\repos\FBDatadriven\FBDatadriven\CsvFile\FBfile.csv";
-        DoAction.DoActions action =new DoAction.DoActions();
-        
+        DoAction.DoActions action = new DoAction.DoActions();
+
         [Test]
         public void Test1()
         {
-            action.LoadUserData(csvFilePath,"Email,Password");
+            test = reports.CreateTest("Tests");
+            test.Log(Status.Info, "Automation FaceBook");
+            action.LoadUserData(csvFilePath, "Email,Password");
+
+            test.Log(Status.Pass, "Test Passes");
+            reports.Flush();
+
         }
+
+       
     }
 }
